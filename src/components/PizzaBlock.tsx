7854dc9a19491ceab32e8b5820269ee034d55292
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-12-12 18:15:10 +0300

    Refactor PizzaBlock component to be dynamic
    This commit restructures the PizzaBlock component to utilize dynamic data rather than static content. Defined types for Pizza, PizzaSize, and PizzaType are now utilized to ensure consistent data structure. The image, title, and price of each pizza are now displayed dynamically. This allows for greater flexibility and scalability as the data for each pizza can be fetched from an API or database instead of being hardcoded, making the management of the menu more efficient.

--- src/components/PizzaBlock.tsx
+++ src/components/PizzaBlock.tsx
@@ -1,13 +1,29 @@
-export const PizzaBlock = () => {
+import { FC } from "react";
+
+// Определение типа для размера пиццы
+type PizzaSize = 26 | 30 | 40;
+
+// Определение типа для типа пиццы
+type PizzaType = 0 | 1;
+
+// Определение типа для объекта пиццы
+type Pizza = {
+  id: number;
+  imageUrl: string;
+  title: string;
+  types: PizzaType[];
+  sizes: PizzaSize[];
+  price: number;
+  category: number;
+  rating: number;
+};
+
+export const PizzaBlock: FC<Pizza> = ({ title, price, imageUrl }) => {
   return (
     <>
       <div className="pizza-block">
-        <img
-          className="pizza-block__image"
-          src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-          alt="Pizza"
-        />
-        <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+        <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+        <h4 className="pizza-block__title">{title}</h4>
         <div className="pizza-block__selector">
           <ul>
             <li className="active">тонкое</li>
@@ -20,7 +36,7 @@ export const PizzaBlock = () => {
           </ul>
         </div>
         <div className="pizza-block__bottom">
-          <div className="pizza-block__price">от 395 ₽</div>
+          <div className="pizza-block__price">от {price} ₽</div>
           <div className="button button--outline button--add">
             <svg
               width="12"
