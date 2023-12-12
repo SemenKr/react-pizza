5428558eb23c2b4049eb615d48f5011376127a4d
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-12-12 18:15:23 +0300

    Refactor PizzaBlock component to be dynamic
    This commit restructures the PizzaBlock component to utilize dynamic data rather than static content. Defined types for Pizza, PizzaSize, and PizzaType are now utilized to ensure consistent data structure. The image, title, and price of each pizza are now displayed dynamically. This allows for greater flexibility and scalability as the data for each pizza can be fetched from an API or database instead of being hardcoded, making the management of the menu more efficient.

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,20 @@ const App = () => {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
+            <PizzaBlock
+              title={"Пепперони Фреш с перцем"}
+              price={803}
+              imageUrl={
+                "https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg"
+              }
+            />
+            <PizzaBlock
+              title={"Сырная"}
+              price={245}
+              imageUrl={
+                "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+              }
+            />
           </div>
         </div>
       </div>
