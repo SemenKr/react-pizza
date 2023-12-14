00eb34ba91ea177030f65b24b4e6a6550ab82a06
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-12-14 16:01:07 +0300

    добавляет клик по категориям товара и содает класс активности

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Categories: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onClickCategoty = (index: number) => {
+    setActiveIndex(index);
+  };
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          onClick={() => onClickCategoty(0)}
+          className={activeIndex === 0 ? "active" : ""}
+        >
+          Все
+        </li>
+        <li
+          onClick={() => onClickCategoty(1)}
+          className={activeIndex === 1 ? "active" : ""}
+        >
+          Мясные
+        </li>
+        <li
+          onClick={() => onClickCategoty(2)}
+          className={activeIndex === 2 ? "active" : ""}
+        >
+          Вегетарианская
+        </li>
+        <li
+          onClick={() => onClickCategoty(3)}
+          className={activeIndex === 3 ? "active" : ""}
+        >
+          Гриль
+        </li>
+        <li
+          onClick={() => onClickCategoty(4)}
+          className={activeIndex === 4 ? "active" : ""}
+        >
+          Острые
+        </li>
+        <li
+          onClick={() => onClickCategoty(5)}
+          className={activeIndex === 5 ? "active" : ""}
+        >
+          Закрытые
+        </li>
       </ul>
     </div>
   );
