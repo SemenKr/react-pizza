40f0355e55f9d51c6c0fe39751166b128dac3ca8
semenkr <samkrekotyn@gmail.com>
2023-12-16 20:18:12 +0300

    #7 добавляет Link для корзины и саму корзину

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <div className="container">
           <Routes>
             <Route path={"/"} element={<Home />} />
+            <Route path={"/cart"} element={<Cart />} />
             <Route path={"*"} element={<NotFound />} />
           </Routes>
         </div>
